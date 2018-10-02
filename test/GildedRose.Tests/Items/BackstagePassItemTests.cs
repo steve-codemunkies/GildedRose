047f2ce8168eab91842e8ee14e28f99bdca2c432
Steve Hocking <steve@codemunki.es>
2018-10-02 22:07:46 +0100

    Move the backstage pass item in to the program

--- test/GildedRose.Tests/Items/BackstagePassItemTests.cs
+++ test/GildedRose.Tests/Items/BackstagePassItemTests.cs
@@ -209,38 +209,4 @@ namespace GildedRose.Tests.Items
             result.Should().Be($"{itemName} {sellIn-1} 0");
         }
     }
-
-    public class BackstagePassItem : BaseItem
-    {
-        public BackstagePassItem(string itemName, int sellIn, int quality) : base(itemName, sellIn, quality)
-        {
-        }
-
-        public override void AgeOneDay()
-        {
-            _sellIn--;
-
-            if(_sellIn > 10)
-            {
-                _quality++;
-            }
-            else if(_sellIn > 5 && _sellIn <= 10)
-            {
-                _quality+=2;
-            }
-            else if (_sellIn >= 0 && _sellIn <= 5)
-            {
-                _quality+=3;
-            }
-            else
-            {
-                _quality = QualityFloorInclusive;
-            }
-
-            if(_quality > QualityCeilingInclusive)
-            {
-                _quality = QualityCeilingInclusive;
-            }
-        }
-    }
 }
