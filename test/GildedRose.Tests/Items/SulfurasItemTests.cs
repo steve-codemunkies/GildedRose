40a4c54ab46536ad402ed5a16fc997d701d444b3
Steve Hocking <steve@codemunki.es>
2018-10-02 21:12:19 +0100

    Add a test for negative quality

--- test/GildedRose.Tests/Items/SulfurasItemTests.cs
+++ test/GildedRose.Tests/Items/SulfurasItemTests.cs
@@ -33,6 +33,22 @@ namespace GildedRose.Tests.Items
             // Assert
             result.Should().Be($"{itemName} {sellIn} {quality}");
         }
+
+        [Fact]
+        public void WhenITryToCreateAnAgedBrieItemWithANegativeQualityItShouldThrowAnArgumentException()
+        {
+            // Arrange
+            var itemName = AutoFixture.Create<string>();
+            var sellIn = AutoFixture.Create<int>();
+            var quality = Random.Next(-1000, 0);
+
+            // Act
+            var exception = Assert.Throws<ArgumentException>(() => new AgedBrieItem(itemName, sellIn, quality));
+
+            // Assert
+            exception.ParamName.Should().Be("quality");
+            exception.Message.Should().StartWith("Cannot be negative");
+        }
     }
 
     public class SulfurasItem : BaseItem
