8b65f737828fa5031b7761ff19a958c31f4d5bf2
Steve Hocking <steve@codemunki.es>
2018-10-02 22:51:38 +0100

    Modified program to use the factory and items

--- src/GildedRose/Program.cs
+++ src/GildedRose/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using GildedRose.Items;
 
 namespace GildedRose
 {
@@ -6,7 +8,32 @@ namespace GildedRose
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Start again");
+            var items = new List<IItem>();
+            IItemFactory itemFactory = new ItemFactory();
+
+            items.Add(itemFactory.Build("Aged Brie", 1, 1));
+            items.Add(itemFactory.Build("Backstage passes", -1, 2));
+            items.Add(itemFactory.Build("Backstage passes", 9, 2));
+            items.Add(itemFactory.Build("Sulfuras", 2, 2));
+            items.Add(itemFactory.Build("Normal Item", -1, 55));
+            items.Add(itemFactory.Build("Normal Item", 2, 2));
+            items.Add(itemFactory.Build("INVALID ITEM", 2, 2));
+            items.Add(itemFactory.Build("Conjured", 2, 2));
+            items.Add(itemFactory.Build("Conjured", -1, 5));
+
+            Console.WriteLine("Test input:");
+            foreach(var item in items)
+            {
+                Console.WriteLine(item);
+                item.AgeOneDay();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Output after aging:");
+            foreach(var item in items)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
