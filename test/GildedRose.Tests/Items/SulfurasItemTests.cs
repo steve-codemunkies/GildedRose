c281a604dd7c2f67c23424524ae3f6588f6f9120
Steve Hocking <steve@codemunki.es>
2018-10-02 21:10:07 +0100

    Add SulfurasItem with ToString test

---
+++ test/GildedRose.Tests/Items/SulfurasItemTests.cs
@@ -0,0 +1,49 @@
+using System;
+using AutoFixture;
+using FluentAssertions;
+using GildedRose.Items;
+using Xunit;
+
+namespace GildedRose.Tests.Items
+{
+    public class SulfurasItemTests
+    {
+        public SulfurasItemTests()
+        {
+            AutoFixture = new Fixture();
+            Random = new Random((int)DateTime.Now.Ticks);
+        }
+
+        public Fixture AutoFixture { get; }
+        public Random Random { get; }
+
+        [Fact]
+        public void WhenICallToStringOnASulfurasItemIGetOutputInTheExpectedFormat()
+        {
+            // Arrange
+            var itemName = AutoFixture.Create<string>();
+            var sellIn = AutoFixture.Create<int>();
+            var quality = Random.Next(0, 1000);
+
+            IItem subject = new SulfurasItem(itemName, sellIn, quality);
+
+            // Act
+            var result = subject.ToString();
+
+            // Assert
+            result.Should().Be($"{itemName} {sellIn} {quality}");
+        }
+    }
+
+    public class SulfurasItem : BaseItem
+    {
+        public SulfurasItem(string itemName, int sellIn, int quality) : base(itemName, sellIn, quality)
+        {
+        }
+
+        public override void AgeOneDay()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
