c4c1a3eca1ff3eae7a99c80cbd063fc4fbcdf21a
Steve Hocking <steve@codemunki.es>
2025-05-27 22:26:00 +0100

    And apply again to the next instace

--- src/GildedRose/GildedRose.cs
+++ src/GildedRose/GildedRose.cs
@@ -30,10 +30,7 @@ public class GildedRose
                 {
                     if (item.SellIn < 11)
                     {
-                        if (item.Quality < MaximumItemQuality)
-                        {
-                            item.Quality = item.Quality + 1;
-                        }
+                        item.Quality = Math.Min(item.Quality + 1, MaximumItemQuality);
                     }
 
                     if (item.SellIn < 6)
