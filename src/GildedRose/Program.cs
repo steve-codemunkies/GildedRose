8c34b7d467a3d703fd6a1d627b1643c78ab2789b
Steve Hocking <steve@codemunki.es>
2025-05-27 21:17:45 +0100

    Convert inner program loop to foreach, clarity, idiomatic code

--- src/GildedRose/Program.cs
+++ src/GildedRose/Program.cs
@@ -53,10 +53,12 @@ public class Program
         {
             Console.WriteLine("-------- day " + i + " --------");
             Console.WriteLine("name, sellIn, quality");
-            for (var j = 0; j < Items.Count; j++)
+
+            foreach (Item item in Items)
             {
-                System.Console.WriteLine(Items[j].Name + ", " + Items[j].SellIn + ", " + Items[j].Quality);
+                System.Console.WriteLine(item.Name + ", " + item.SellIn + ", " + item.Quality);
             }
+
             Console.WriteLine("");
             app.UpdateQuality();
         }
