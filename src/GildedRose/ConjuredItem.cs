f45b678c8423317fad2212079550a70e57f2f38a
Steve Hocking <steve@codemunki.es>
2025-05-28 19:29:04 +0100

    Implement UpdateItemQuality on ConjuredItem

--- src/GildedRose/ConjuredItem.cs
+++ src/GildedRose/ConjuredItem.cs
@@ -1,10 +1,18 @@
-﻿namespace GildedRoseKata
+﻿using static GildedRoseKata.Item;
+
+namespace GildedRoseKata
 {
     public class ConjuredItem : Item
     {
+        private const int MinimumItemQuality = 0;
+
         public override void UpdateItemQuality()
         {
-            throw new NotImplementedException();
+            SellIn = SellIn - 1;
+
+            var decrement = SellIn < 0 ? 4 : 2;
+
+            Quality = Math.Max(Quality - decrement, MinimumItemQuality);
         }
     }
 }
