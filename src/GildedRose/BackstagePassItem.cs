82ad1050494640ab1ab8f3768cd965ade738816e
Steve Hocking <steve@codemunki.es>
2025-05-28 19:10:16 +0100

    Implement UpdateItemQuality on bs pass

--- src/GildedRose/BackstagePassItem.cs
+++ src/GildedRose/BackstagePassItem.cs
@@ -2,9 +2,20 @@
 {
     public class BackstagePassItem : Item
     {
+        private const int MinimumItemQuality = 0;
+        private const int MaximumItemQuality = 50;
+
         public override void UpdateItemQuality()
         {
-            throw new NotImplementedException();
+            SellIn = SellIn - 1;
+
+            Quality = SellIn switch
+            {
+                < 0 => MinimumItemQuality,
+                < 6 => Math.Min(Quality + 3, MaximumItemQuality),
+                < 11 => Math.Min(Quality + 2, MaximumItemQuality),
+                _ => Math.Min(Quality + 1, MaximumItemQuality),
+            };
         }
     }
 }
