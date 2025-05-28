badb9b9671d187f83401964e73872173a2ed8c6e
Steve Hocking <steve@codemunki.es>
2025-05-28 13:51:33 +0100

    Add passing tests for normal items

--- src/GildedRoseTests/GildedRoseTest.cs
+++ src/GildedRoseTests/GildedRoseTest.cs
@@ -20,4 +20,22 @@ public class GildedRoseTest
         Assert.Equal(expectedQuality, item.Quality);
         Assert.Equal(expectedSellIn, item.SellIn);
     }
+
+    [Theory]
+    [InlineData(10, 20, 9, 19)]
+    [InlineData(10, 1, 9, 0)]
+    [InlineData(10, 0, 9, 0)]
+    [InlineData(1, 10, 0, 9)]
+    [InlineData(0, 9, -1, 7)]
+    [InlineData(-4, 1, -5, 0)]
+    public void NormalItemDecreasesSellInAndQuality(int initialSellIn, int initialQuality, int expectedSellIn, int expectedQuality)
+    {
+        var item = new Item { Name = "Normal Item", SellIn = initialSellIn, Quality = initialQuality };
+
+        var app = new GildedRose([item]);
+        app.UpdateQuality();
+
+        Assert.Equal(expectedSellIn, item.SellIn);
+        Assert.Equal(expectedQuality, item.Quality);
+    }
 }
