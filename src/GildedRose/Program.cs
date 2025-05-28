3881290ebb8a85b97699e7a93cdf7bd8e1bd3a2e
Steve Hocking <steve@codemunki.es>
2025-05-28 17:56:20 +0100

    Update the main program to use the new LegendaryItem factory

--- src/GildedRose/Program.cs
+++ src/GildedRose/Program.cs
@@ -14,8 +14,8 @@ public class Program
             new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 },
             new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 },
             new Item { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7 },
-            new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 },
-            new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80 },
+            ItemFactory.LegendaryItem("Sulfuras, Hand of Ragnaros", 80, 0),
+            ItemFactory.LegendaryItem("Sulfuras, Hand of Ragnaros", 80, -1),
             new Item
             {
                 Name = "Backstage passes to a TAFKAL80ETC concert",
