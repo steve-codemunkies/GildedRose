3be7f6898b98aed37c34a53bf1ca42cb8472ccb9
Steve Hocking <steve@codemunki.es>
2025-05-28 15:35:30 +0100

    Add failing tests for conjured mana
    Red-green-refactor.

--- src/GildedRoseTests/GildedRoseTest.cs
+++ src/GildedRoseTests/GildedRoseTest.cs
@@ -93,4 +93,26 @@ public class GildedRoseTest
         Assert.Equal(expectedSellIn, item.SellIn);
         Assert.Equal(expectedQuality, item.Quality);
     }
+
+    [Theory]
+    [InlineData(10, 20, 9, 18)]
+    [InlineData(10, 2, 9, 0)]
+    [InlineData(10, 1, 9, 0)]
+    [InlineData(10, 0, 9, 0)]
+    [InlineData(1, 10, 0, 8)]
+    [InlineData(0, 9, -1, 5)]
+    [InlineData(-4, 3, -5, 0)]
+    [InlineData(-4, 2, -5, 0)]
+    [InlineData(-4, 1, -5, 0)]
+    [InlineData(-4, 0, -5, 0)]
+    public void ConjuredItemsDecreasesQualityAtDoubleSpeedAndSellInAtNormalSpeed(int initialSellIn, int initialQuality, int expectedSellIn, int expectedQuality)
+    {
+        var item = new Item { Name = "Conjured Mana Cake", SellIn = initialSellIn, Quality = initialQuality };
+
+        var app = new GildedRose([item]);
+        app.UpdateQuality();
+
+        Assert.Equal(expectedSellIn, item.SellIn);
+        Assert.Equal(expectedQuality, item.Quality);
+    }
 }
