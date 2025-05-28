e94e2069125b6e340aef36d3557492b71df1316b
Steve Hocking <steve@codemunki.es>
2025-05-28 17:27:50 +0100

    Move quality logic to item

--- src/GildedRose/GildedRose.cs
+++ src/GildedRose/GildedRose.cs
@@ -2,83 +2,11 @@
 
 public class GildedRose(IList<Item> Items)
 {
-    private const int MinimumItemQuality = 0;
-    private const int MaximumItemQuality = 50;
-    private const int SulfurasItemQuality = 80;
-
-    public enum ItemType
-    {
-        NormalItem,
-        QualityIncrementItem,
-        BackstagePasses,
-        LegendaryItem,
-        ConjuredItem
-    }
-
     public void UpdateQuality()
     {
         foreach (Item item in Items)
         {
-            UpdateItemQuality(item);
+            item.UpdateItemQuality();
         }
-    }
-
-    private void UpdateItemQuality(Item item)
-    {
-        var itemType = GetItemType(item);
-
-        if (itemType == ItemType.LegendaryItem)
-        {
-            item.Quality = SulfurasItemQuality;
-            return;
-        }
-
-        item.SellIn = item.SellIn - 1;
-
-        switch (itemType)
-        {
-            case ItemType.QualityIncrementItem:
-                var increment = item.SellIn < 0 ? 2 : 1;
-
-                item.Quality = Math.Min(item.Quality + increment, MaximumItemQuality);
-                break;
-
-            case ItemType.BackstagePasses:
-                item.Quality = item.SellIn switch
-                {
-                    < 0 => MinimumItemQuality,
-                    < 6 => Math.Min(item.Quality + 3, MaximumItemQuality),
-                    < 11 => Math.Min(item.Quality + 2, MaximumItemQuality),
-                    _ => Math.Min(item.Quality + 1, MaximumItemQuality),
-                };
-                break;
-
-            case ItemType.ConjuredItem:
-                var decrement = item.SellIn < 0 ? 4 : 2;
-
-                item.Quality = Math.Max(item.Quality - decrement, MinimumItemQuality);
-                break;
-
-            case ItemType.NormalItem:
-            default:
-                decrement = item.SellIn < 0 ? 2 : 1;
-
-                item.Quality = Math.Max(item.Quality - decrement, MinimumItemQuality);
-                break;
-        }
-
-        return;
-    }
-
-    private ItemType GetItemType(Item item)
-    {
-        return item.Name switch
-        {
-            "Sulfuras, Hand of Ragnaros" => ItemType.LegendaryItem,
-            "Aged Brie" => ItemType.QualityIncrementItem,
-            "Backstage passes to a TAFKAL80ETC concert" => ItemType.BackstagePasses,
-            "Conjured Mana Cake" => ItemType.ConjuredItem,
-            _ => ItemType.NormalItem
-        };
     }
 }
--- src/GildedRose/Item.cs
+++ src/GildedRose/Item.cs
@@ -5,4 +5,77 @@ public class Item
     public string Name { get; set; }
     public int SellIn { get; set; }
     public int Quality { get; set; }
+
+    private const int MinimumItemQuality = 0;
+    private const int MaximumItemQuality = 50;
+    private const int SulfurasItemQuality = 80;
+
+    public enum ItemType
+    {
+        NormalItem,
+        QualityIncrementItem,
+        BackstagePasses,
+        LegendaryItem,
+        ConjuredItem
+    }
+
+    public void UpdateItemQuality()
+    {
+        var itemType = GetItemType();
+
+        if (itemType == ItemType.LegendaryItem)
+        {
+            Quality = SulfurasItemQuality;
+            return;
+        }
+
+        SellIn = SellIn - 1;
+
+        switch (itemType)
+        {
+            case ItemType.QualityIncrementItem:
+                var increment = SellIn < 0 ? 2 : 1;
+
+                Quality = Math.Min(Quality + increment, MaximumItemQuality);
+                break;
+
+            case ItemType.BackstagePasses:
+                Quality = SellIn switch
+                {
+                    < 0 => MinimumItemQuality,
+                    < 6 => Math.Min(Quality + 3, MaximumItemQuality),
+                    < 11 => Math.Min(Quality + 2, MaximumItemQuality),
+                    _ => Math.Min(Quality + 1, MaximumItemQuality),
+                };
+                break;
+
+            case ItemType.ConjuredItem:
+                var decrement = SellIn < 0 ? 4 : 2;
+
+                Quality = Math.Max(Quality - decrement, MinimumItemQuality);
+                break;
+
+            case ItemType.NormalItem:
+            default:
+                decrement = SellIn < 0 ? 2 : 1;
+
+                Quality = Math.Max(Quality - decrement, MinimumItemQuality);
+                break;
+        }
+
+        return;
+    }
+
+    private ItemType GetItemType()
+    {
+        return Name switch
+        {
+            "Sulfuras, Hand of Ragnaros" => ItemType.LegendaryItem,
+            "Aged Brie" => ItemType.QualityIncrementItem,
+            "Backstage passes to a TAFKAL80ETC concert" => ItemType.BackstagePasses,
+            "Conjured Mana Cake" => ItemType.ConjuredItem,
+            _ => ItemType.NormalItem
+        };
+    }
+
 }
